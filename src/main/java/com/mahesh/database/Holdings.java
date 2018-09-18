cd5403fcb2c2e8547d6c7cdd389338645f9bc739
vnemana <venkat.nemana@gmail.com>
2018-09-17 21:36:54 -0700

    Added database accessor for Filings

--- src/main/java/com/mahesh/database/Holdings.java
+++ src/main/java/com/mahesh/database/Holdings.java
@@ -2,9 +2,44 @@ package com.mahesh.database;
 
 import com.mahesh.utilities.HoldingRecord;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Holdings {
+    int fundId;
+    int filingId;
+    int holdingId;
+    String cusip;
+    String stock;
+    double position;
+    int numShares;
+
+    public void insert(int fundId, int filingId, String cusip, String stock, double position, int numShares) throws SQLException {
+        try {
+            FundReportsDb db = new FundReportsDb();
+            Connection conn = db.getConn();
+
+            String insertString = "insert into Holding (fundId, filingId, cusip, stock, position, numshares) " +
+                    "values (?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(insertString);
+            preparedStatement.setInt(1, fundId);
+            preparedStatement.setInt(2, filingId);
+            preparedStatement.setString(3, cusip);
+            preparedStatement.setString(4, stock);
+            preparedStatement.setDouble(5, position);
+            preparedStatement.setInt(6, numShares);
+
+            preparedStatement.execute();
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw e;
+        }
+    }
 
     //If Fund already exists, then retrieve fund id from fund table
     //  Else Insert into Fund table
